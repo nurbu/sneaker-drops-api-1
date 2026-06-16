d057cc57d86224ed1e5f315ed9544815e05a65a1
Nurbu <lamanurbu2@gmail.com>
2026-06-16 11:25:17 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/sneakerdropsapi/SneakerDropsApi1Application.java
@@ -0,0 +1,13 @@
+package com.pluralsight.sneakerdropsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerDropsApi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakerDropsApi1Application.class, args);
+    }
+
+}
